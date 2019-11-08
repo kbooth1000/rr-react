afe0292abee0d0125291aa526b07c0f3d28b51e2
Kyle Booth <kbooth1000@gmail.com>
2019-11-07 23:29:18 -0500

    Add unstyled Heating/Cooling pages

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,8 @@ import { Route } from 'react-router-dom';
 import Header from './Header';
 import HomePage from './HomePage';
 import SystemRec from './SystemRec';
+import Heating from './Heating';
+import Cooling from './Cooling';
 import ForSale from './ForSale';
 import Financing from './Financing';
 import Quote from './Quote';
@@ -14,6 +16,8 @@ const Main = () => {
       <Header />
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={SystemRec} />
+      <Route path='/heating' component={Heating} />
+      <Route path='/cooling' component={Cooling} />
       <Route exact path='/equipment-for-sale' component={ForSale} />
       <Route exact path='/financing' component={Financing} />
       <Route exact path='/quote' component={Quote} />
