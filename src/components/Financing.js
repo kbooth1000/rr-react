df6c54d2c80d7f31c7d4272a64f4ca4de5031348
ronfletcher1 <ronfletcher1@me.com>
2019-11-11 19:06:45 -0500

    add finincing page; no changes to package-lock.json

--- src/components/Financing.js
+++ src/components/Financing.js
@@ -4,15 +4,58 @@ const Financing = () => {
   return (
     <div className="Financing">
 
-      <div> <p>GET FINANCING</p>
-        <p><i>Heat Pumps are Common in the South and Provide heat and air conditioning</i></p>
-        <p><i>Both the electric furnace and the heat pump use all electric 
-              to produce heat. However, they do it in different ways and the heat 
-              pump, if properly installed, is far more efficient. More efficient 
-              when operating in the heat pump mode than the electric furnace. It may 
-              sound contradictory when I tell you that electric heat for the electric 
-              furnace is 100% efficient. All of the heat the electric furnace produces is 
-              used to heat your home.</i></p>
+      <div> <p>HVAC FINANCING 101</p>
+        <p><i>For most people, HVAC financing is a large expense, and can be challenging 
+              for anyone, especially a new homeowner. While paying out of pocket is the 
+              best option, there are plenty of options to finance the HVAC system that 
+              is right for you.</i></p>
+        <p><i>The cost to install a new HVAC system can vary based on:
+
+              The type of equipment being installed (full system or replacement parts);
+              The existing equipment (age and location);
+              And the type of install (new install or replace existing)</i></p>
+        <p><i>What to Know Before You Buy</i></p>
+        <p><i>Home Equity
+              Borrowing against the equity in your home can often be a good way to 
+              finance home improvements. This option has less interest and is tax-deductible, 
+              but there are risks. Falling behind on your payment could cost you not only 
+              your HVAC system but your home.
+
+              <i>Private Lenders</i>
+              You can also borrow from private lenders, such as banks or credit unions. 
+              The interest rates and max amounts available to borrow will vary, but many banks 
+              have low-interest rates for these types of loans.
+
+              Loan Programs
+              There are loan programs, such as the Federal Housing Administration’s (FHA) 
+              PowerSaver Loan Program, that allow you to borrow up to $7,500 without putting 
+              up your home as collateral. However, you must have home equity to participate.
+
+              Credit Card Companies
+              Talk to your credit card company about charging this large of a purchase to 
+              your card. Know that there is a possibility of higher interest rates.
+
+              Utility Companies
+              Utility companies can also help by charging monthly payments for your new HVAC 
+              system on your electric bill. They often provide low-interest rates if you choose 
+              energy-efficient systems.</i></p>
+              <div class="cell finance-btn1">
+                <div class="12-month-button">
+                    <a rel="noopener noreferrer" target="_blank" href="https://application.enerbank.com/#/link?spnum=8665104594&amp;cid=10021&amp;promo=RE468&amp;loamt=" 
+                      class="apply-button">12 Month <span>Same-As-Cash*</span><aside class="hover-show">*Loans provided by EnerBank USA, Member FDIC, (1245 Brickyard Rd., 
+                      Suite 600, Salt Lake City, UT 84106) on approved credit, for a limited time. Repayment terms vary from 24 to 132 months. Interest waived if repaid 
+                      in 365 days. 18.58% fixed APR, effective as of August 1, 2019, subject to change. Interest starts accruing when the loan closes.</aside></a>
+                </div>
+              </div>
+              <div class="cell finance-btn2">
+                <div class="60-month-button">
+                    <a rel="noopener noreferrer" target="_blank" href="https://application.enerbank.com/#/link?spnum=8665104594&amp;cid=10021&amp;promo=ZRE78&amp;loamt=" 
+                      class="apply-button">0.00%<span>60-Month Loan*</span><aside class="hover-show">*Loans provided by EnerBank USA, Member FDIC, (1245 Brickyard Rd., 
+                      Suite 600, Salt Lake City, UT 84106) on approved credit, for a limited time. Repayment terms vary from 24 to 132 months. Interest waived if repaid 
+                      in 365 days. 18.58% fixed APR, effective as of August 1, 2019, subject to change. Interest starts accruing when the loan closes.</aside></a>
+                </div>
+              </div>
+
       </div>
     </div>
   )
