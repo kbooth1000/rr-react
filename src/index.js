06dc4acac3c625c77c12af357ccf174ccc905f02
Kyle Booth <kbooth1000@gmail.com>
2020-01-26 02:24:14 -0500

    Change logo img ref in css

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import { hydrate, render } from 'react-dom';
 import './index.css';
 import App from './App';
 
-// const rootElement = document.getElementById("root");
-// if (rootElement.hasChildNodes()) {
-//   hydrate(<App />, rootElement);
-// } else {
-//   render(<App />, rootElement);
-// }
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 
